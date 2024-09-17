e9a223b8dcfa72d7deec08bfd3901677d1e0b387
Lautaro Chaparro <lautaro.chaparro.et21.21@gmail.com>
2024-09-17 18:29:23 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const app = require('./presentation/app')
 const database = require('./data/database/database')
 const bcrypt = require('bcryptjs')
